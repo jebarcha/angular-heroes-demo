a71f9856ad89babebc06e42487283206134879e9
Jebarcha <jebarcha@hotmail.com>
2019-07-02 11:31:28 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+
+// Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
+@NgModule({
+	declarations: [ AppComponent, NavbarComponent, HomeComponent, AboutComponent, HeroesComponent, HeroeComponent, BuscadorComponent, HeroeTarjetaComponent ],
+	imports: [ BrowserModule, FormsModule, APP_ROUTING ],
+	providers: [],
+	bootstrap: [ AppComponent ]
+})
+export class AppModule {}
